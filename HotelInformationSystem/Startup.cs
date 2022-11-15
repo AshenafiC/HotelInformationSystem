3db4554d205f388041a2842f65f38948ba6d0d84
AshenafiC <achalchissa@excellerentsolutions.com>
2022-11-15 14:34:53 +0300

    Add Country and Hotel Get Methods. Also fixed Unit of work Dependency Injection reference.

--- HotelInformationSystem/Startup.cs
+++ HotelInformationSystem/Startup.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
 using HotelInformationSystem.Configurations;
 using HotelInformationSystem.Data;
+using HotelInformationSystem.IRepository;
+using HotelInformationSystem.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -44,12 +46,16 @@ namespace HotelInformationSystem
 
             services.AddAutoMapper(typeof(MapperInitializer));
 
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Hotel Information System", Version = "v1" });
             });
 
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(op =>
+                op.SerializerSettings.ReferenceLoopHandling = 
+                    Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
